346b59e978954aa5714f9c956be7fc88d892547f
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-09 13:46:34 +0300

    program cs

--- WebApiUnitTestUdemy/Program.cs
+++ WebApiUnitTestUdemy/Program.cs
@@ -6,9 +6,8 @@ using WebApiUnitTestUdemy.Repositories.Concrete;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
-builder.Services.AddScoped<IProductRepositoy,ProductRepository>();
+builder.Services.AddDbContext<ApplicationDbContext>(b => b.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
