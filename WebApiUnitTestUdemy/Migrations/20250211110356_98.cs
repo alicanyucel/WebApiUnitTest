5eca94a538396cdd25befb32d1e2f8d91c84e1c1
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-11 14:22:51 +0300

    product controller fiil

---
+++ WebApiUnitTestUdemy/Migrations/20250211110356_98.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiUnitTestUdemy.Migrations
+{
+    /// <inheritdoc />
+    public partial class _98 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ WebApiUnitTestUdemy/Models/Helper.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiUnitTestUdemy.Models
+{
+    public class Helpers
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
